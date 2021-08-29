78eb51585cc13343ff72354745bae109cdeed19a
hatem <hatemhtm7@gmail.com>
2021-08-29 19:59:33 +0300

    map screen

--- components/NavObtions.js
+++ components/NavObtions.js
@@ -1,8 +1,10 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, FlatList, TouchableOpacity, View ,Image} from 'react-native';
+import { StyleSheet, Text, FlatList, TouchableOpacity, View, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
+import { selectOrigin } from '../slices/navSlice';
 
 const data = [
   {
@@ -20,35 +22,37 @@ const data = [
 ];
 
 const NavObtions = () => {
+  const origin = useSelector(selectOrigin)
 
   const navigation = useNavigation()
   return (
     <FlatList
-    data={data}
-    horizontal
-    keyExtractor={(item) => item.id}
-    renderItem={({item})=>(
-      <TouchableOpacity
-      onPress={()=> navigation.navigate(item.screen)}
-      style={tw`p-2 p-6 p-8 p-4 bg-gray-200 m-2 w-40` }
-      >
-        <View>
-          <Image
-          style={{width:120,height:120,resizeMode:'contain'}}
-          source={{uri:item.image}}
-          />
-        </View>
-        <Text style={tw`mt-2 text-lg font-semibold`} >{item.title}</Text>
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 p-6 p-8 p-4 bg-gray-200 m-2 w-40`}
+          disabled={!origin}
+        >
+          <View style={tw`${!origin && 'opacity-20'}`}>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: 'contain' }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`} >{item.title}</Text>
 
-        <Icon
-          style={tw`p-2 bg-black rounded-full w-10 mt-4`}
-          name='arrowright'
-          color='white'
-          type='antdesign'
-        />
-      </TouchableOpacity>
-    )}
-    
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name='arrowright'
+              color='white'
+              type='antdesign'
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+
     />
   );
 };
---
+++ components/NavigateCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const NavigateCard = () => {
+  return (
+    <SafeAreaView>
+      <Text>Weeee!</Text>
+    </SafeAreaView>
+  )
+}
+
+export default NavigateCard
+
+const styles = StyleSheet.create({})
